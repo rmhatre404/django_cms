5998a7d85091c4231ff0fbee36d0f40fe2054591
rmhatre404 <rmhatre404@gmail.com>
2025-01-21 23:27:19 +0530

    testing git

--- cms_project/settings.py
+++ cms_project/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/10081997
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-6apmt4p$xo0o(-2*#nm7pd-m_3oouolyp(u$sgwic%vcw38*^o"
